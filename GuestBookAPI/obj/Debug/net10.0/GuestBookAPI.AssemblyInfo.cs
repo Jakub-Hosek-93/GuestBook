5c69deeb2966ab86b80f0e1eb69a0e8e6a867e3a
hosek.jakub <hosek.jakub@ssakhk.cz>
2026-04-28 21:24:06 +0200

    style: update page styles

--- GuestBookAPI/obj/Debug/net10.0/GuestBookAPI.AssemblyInfo.cs
+++ GuestBookAPI/obj/Debug/net10.0/GuestBookAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuestBookAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6f34d9887556c12bb4c7fb9ccdc3cac33b3b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuestBookAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuestBookAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
