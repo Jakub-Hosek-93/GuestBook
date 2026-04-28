8d6f34d9887556c12bb4c7fb9ccdc3cac33b3b2e
hosek.jakub <hosek.jakub@ssakhk.cz>
2026-04-28 19:11:49 +0200

    Innitial commit

---
+++ GuestBookAPI/Controllers/GuestBookController.cs
@@ -0,0 +1,34 @@
+using GuestBookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBookAPI.Controllers;
+
+[ApiController]
+[Route("")]
+public class GuestBookController : Controller
+{
+    // Database
+    private static List<RecordModel> _guestBookEntries = new()
+    {
+        new RecordModel { ID = 1, Name = "Systém", Text = "Vítejte v naší knize vzkazů!", Date =  new DateTime(2021, 05, 01) },
+        new RecordModel { ID = 2, Name = "Admin", Text = "Tohle je testovací zpráva.", Date = new DateTime(2021, 09, 01) },
+    };
+    
+    [HttpGet]
+    public List<RecordModel> ShowGuestBookEntries()
+    {
+        return _guestBookEntries;
+    }
+    
+    [HttpPost]
+    public RecordModel AddRecord(RecordModel record)
+    {
+        record.ID = _guestBookEntries.Count + 1;
+        record.Date = DateTime.Now;
+        
+        _guestBookEntries.Add(record);
+
+        return record;
+
+    }
+}
---
+++ GuestBookAPI/Models/RecordModel.cs
@@ -0,0 +1,9 @@
+namespace GuestBookAPI.Models;
+
+public class RecordModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; }
+}
