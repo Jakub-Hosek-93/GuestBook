81d24ef2d3d75f9ecc69a7e24f4a755b3124ef92
hosek.jakub <hosek.jakub@ssakhk.cz>
2026-05-01 21:01:00 +0200

    feat: js NewEntry function

--- GuestBookAPI/wwwroot/script.js
+++ GuestBookAPI/wwwroot/script.js
@@ -1,19 +1,18 @@
-
 apiURL = "/";
 
 // funkce na Načtení
 async function LoadGuestBookAPI() {
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiURL);
         const data = await response.json();
-        
-        const tableBody = document.getElementById('guestbook-entries');
+
+        const tableBody = document.getElementById('guestbook-table');
         tableBody.innerHTML = '';
 
         data.forEach(item => {
             const row = document.createElement('tr');
             const date = new Date(item.date).toLocaleString('cs-CZ');
-            
+
             row.innerHTML = `
                         <td class="name-col">${item.name}</td>
                         <td class="text-col">${item.text}</td>
@@ -25,10 +24,39 @@ async function LoadGuestBookAPI() {
     catch (err){
         console.error("Chyba:", err)
     }
-    
-    
+
+
 }
 
 // funkce na Přidáni
 async function NewEntry(){
+    const nameInput = document.getElementById('name').value;
+    const textInput = document.getElementById('message').value;
+
+    if (!nameInput || !textInput) {
+        alert("Vyplňte prosím jméno i vzkaz.");
+        return;
+    }
+
+    const entry = {
+        name: nameInput,
+        text: textInput
+    };
+
+    try {
+        await fetch(apiURL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(entry)
+        });
+
+        document.getElementById('name').value = '';
+        document.getElementById('message').value = '';
+        
+        LoadGuestBookAPI();
+    }catch(err){
+        console.error("Chyba:", err)
+    }
 }
+
+LoadGuestBookAPI();
